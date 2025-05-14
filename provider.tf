f5b75f8140cbc46eee05ef178e03eb0ab24c52f6
Peter T. <tpetera@pm.me>
2025-05-14 16:31:20 +0200

    added skip_provider_registration

--- provider.tf
+++ provider.tf
@@ -10,14 +10,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  # For GitHub Actions:
-  # Authentication will be handled by the `azure/login@v1` action using OIDC.
-  # The Service Principal (whose details are used by OIDC) needs 'Contributor'
-  # role on the resource group and 'Storage Blob Data Contributor' on the storage account.
-
-  # For local execution (e.g., running `terraform plan` on your machine):
-  # 1. Make sure you are logged in via Azure CLI (`az login`). Terraform will use these credentials.
-  # 2. Alternatively, you could set environment variables for Service Principal authentication
-  #    (ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_SUBSCRIPTION_ID, ARM_TENANT_ID) if not using OIDC locally.
-  #    However, for OIDC with the backend, we'll configure that in backend.tf.
+  # Skip attempting to register Azure Resource Providers.
+  # This is useful if the Service Principal does not have subscription-level
+  # permissions to register providers.
+  # Ensure that necessary providers (e.g., Microsoft.Compute, Microsoft.Network)
+  # are already registered in your Azure subscription.
+  skip_provider_registration = true
 }
